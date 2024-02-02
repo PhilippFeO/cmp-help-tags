06b1718acf1bada614b6ca42b499dd6c4cf5804b
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-02-02 19:24:48 +0100

    ✨ Feat: Syntax completion for help tags

---
+++ README.md
@@ -0,0 +1,22 @@
+# cmp-help-tags
+Syntax completion for all help tags in Neovim (including your plugins). It uses the same data structure as `:Telescope help_tags`. The plugin targets [nvim-cmp](https://github.com/hrsh7th/nvim-cmp).
+
+## Installation (Lazy.nvim)
+```lua
+{
+  'PhilippFeO/cmp-csv',
+  config = true -- Triggers setup(), necessary
+}
+```
+
+## Enabling within [nvim-cmp](https://github.com/hrsh7th/nvim-cmp)
+```lua
+require("cmp").setup({
+  sources = {
+    { name = "cmp_help_tags",-- '_' not '-' 😯
+      -- recommended to avoid cluttering
+      -- keyword_length = 5
+    },
+  }
+})
+```
---
+++ lua/cmp-help-tags/init.lua
@@ -0,0 +1,154 @@
+local Path = require "plenary.path"
+local utils = require "telescope.utils"
+
+local M = {}
+
+M.cmp_items = {}
+
+M.setup = function()
+  local langs = { 'en' }
+  local langs_map = {}
+  for _, lang in ipairs(langs) do
+    langs_map[lang] = true
+  end
+
+  local tag_files = {}
+  local function add_tag_file(lang, file)
+    if langs_map[lang] then
+      if tag_files[lang] then
+        table.insert(tag_files[lang], file)
+      else
+        tag_files[lang] = { file }
+      end
+    end
+  end
+
+  local help_files = {}
+  local all_files = vim.api.nvim_get_runtime_file("doc/*", true)
+  for _, fullpath in ipairs(all_files) do
+    local file = utils.path_tail(fullpath)
+    if file == "tags" then
+      add_tag_file("en", fullpath)
+    elseif file:match "^tags%-..$" then
+      local lang = file:sub(-2)
+      add_tag_file(lang, fullpath)
+    else
+      help_files[file] = fullpath
+    end
+  end
+
+  local tags = {}
+  local tags_map = {}
+  local delimiter = string.char(9)
+  for _, lang in ipairs(langs) do
+    for _, file in ipairs(tag_files[lang] or {}) do
+      local lines = vim.split(Path:new(file):read(), "\n", true)
+      for _, line in ipairs(lines) do
+        -- TODO: also ignore tagComment starting with ';'
+        if not line:match "^!_TAG_" then
+          local fields = vim.split(line, delimiter, true)
+          if #fields == 3 and not tags_map[fields[1]] then
+            if fields[1] ~= "help-tags" or fields[2] ~= "tags" then
+              table.insert(tags, {
+                name = fields[1],
+                filename = help_files[fields[2]],
+                cmd = fields[3],
+                lang = lang,
+              })
+              tags_map[fields[1]] = true
+            end
+          end
+        end
+      end
+    end
+  end
+
+  --[[
+    Example of element in table tags:
+      {
+        cmd = "/*lazy.nvim-lazy.nvim-installation*",
+        filename = "/home/philipp/.local/share/nvim/lazy/lazy.nvim/doc/lazy.nvim.txt"
+        lang ="en",
+        name = "lazy.nvim-lazy.nvim-installation"
+      }
+  --]]
+
+  for _, ht in ipairs(tags) do
+    table.insert(M.cmp_items, {
+      label = ht.name,
+      documentation = {
+        kind = "text",
+        value = string.format('%s\n\nFilename:%s', ht.name, ht.filename)
+      }
+    })
+  end
+
+  local source = {}
+
+  source.new = function()
+    local self = setmetatable({ cache = {} }, { __index = source })
+
+    return self
+  end
+
+  source.is_available = function()
+    return vim.bo.filetype == 'markdown' or vim.bo.filetype == 'lua'
+  end
+
+  -- source.get_trigger_characters = function()
+  --   return { "??" }
+  -- end
+
+  -- source.get_keyword_pattern = function()
+  --   return 'lorem'
+  -- end
+
+  source.complete = function(_, _, callback)
+    callback(M.cmp_items)
+  end
+
+  require('cmp').register_source("cmp_help_tags", source.new())
+
+
+  -- pickers
+  --     .new(opts, {
+  --       prompt_title = "Help",
+  --       finder = finders.new_table {
+  --         results = tags,
+  --         entry_maker = function(entry)
+  --           return make_entry.set_default_entry_mt({
+  --             value = entry.name .. "@" .. entry.lang,
+  --             display = entry.name,
+  --             ordinal = entry.name,
+  --             filename = entry.filename,
+  --             cmd = entry.cmd,
+  --           }, opts)
+  --         end,
+  --       },
+  --       previewer = previewers.help.new(opts),
+  --       sorter = conf.generic_sorter(opts),
+  --       attach_mappings = function(prompt_bufnr)
+  --         action_set.select:replace(function(_, cmd)
+  --           local selection = action_state.get_selected_entry()
+  --           if selection == nil then
+  --             utils.__warn_no_selection "builtin.help_tags"
+  --             return
+  --           end
+  --
+  --           actions.close(prompt_bufnr)
+  --           if cmd == "default" or cmd == "horizontal" then
+  --             vim.cmd("help " .. selection.value)
+  --           elseif cmd == "vertical" then
+  --             vim.cmd("vert help " .. selection.value)
+  --           elseif cmd == "tab" then
+  --             vim.cmd("tab help " .. selection.value)
+  --           end
+  --         end)
+  --
+  --         return true
+  --       end,
+  --     })
+  --     :find()
+end
+
+return M
